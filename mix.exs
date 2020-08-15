9a6da640b901aecfe38c71380ac09ae23347a9ad
Justin Workman <xtagon@gmail.com>
2020-08-15 09:43:42 -0700

    Install and configure ExDoc.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Snek.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Snek",
+      source_url: "https://github.com/xtagon/snek",
+      homepage_url: "https://github.com/xtagon/snek",
+      docs: [
+        main: "Snek",
+        extras: ["README.md", "LICENSE.txt"]
+      ]
     ]
   end
 
@@ -21,7 +30,8 @@ defmodule Snek.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
     ]
   end
 end
