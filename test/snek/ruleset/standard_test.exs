98d108f6747df5fcdb7d1ad1eb4f8c0a3504dbde
Justin Workman <xtagon@gmail.com>
2020-08-29 20:37:59 -0700

    Add some property tests for next board states in Standard ruleset.

--- test/snek/ruleset/standard_test.exs
+++ test/snek/ruleset/standard_test.exs
@@ -14,6 +14,19 @@ defmodule StandardRulesetTest do
   @custom_board_size_range 3..25
   @custom_snakes_per_game 1..5
 
+  @fixed_8_snake_ids [
+    "snek1",
+    "snek2",
+    "snek3",
+    "snek4",
+    "snek5",
+    "snek6",
+    "snek7",
+    "snek8"
+  ]
+
+  @snake_move_directions [:north, :south, :east, :west]
+
   def setup_for_standard_board_sizes(_context) do
     %{
       board_sizes: StreamData.member_of(@standard_board_sizes),
@@ -205,6 +218,105 @@ defmodule StandardRulesetTest do
         unique_occupied_points = Enum.uniq(occupied_points)
 
         assert length(occupied_points) == length(unique_occupied_points)
+      end
+    end
+  end
+
+  describe "next/2 for standard board sizes with 8 snakes after the first turn" do
+    setup :setup_for_standard_board_sizes
+
+    property "all alive snakes still have a body length of at least 3", context do
+      check all board_size <- context.board_sizes,
+        {:ok, board0} <- StreamData.constant(Standard.init(board_size, @fixed_8_snake_ids)),
+        moves1 <- StreamData.list_of(StreamData.member_of(@snake_move_directions), length: 8),
+        snake_moves1 <- StreamData.constant(Enum.zip(@fixed_8_snake_ids, moves1)),
+        board1 <- StreamData.constant(Standard.next(board0, snake_moves1))
+      do
+        alive_snakes = Enum.filter(board1.snakes, &Snake.alive?/1)
+
+        assert Enum.all?(alive_snakes, fn snake ->
+          length(snake.body) >= 3
+        end)
+      end
+    end
+
+    property "all alive snakes have moved their heads", context do
+      check all board_size <- context.board_sizes,
+        {:ok, board0} <- StreamData.constant(Standard.init(board_size, @fixed_8_snake_ids)),
+        moves1 <- StreamData.list_of(StreamData.member_of(@snake_move_directions), length: 8),
+        snake_moves1 <- StreamData.constant(Enum.zip(@fixed_8_snake_ids, moves1)),
+        board1 <- StreamData.constant(Standard.next(board0, snake_moves1))
+      do
+        alive_snakes = Enum.filter(board1.snakes, &Snake.alive?/1)
+
+        assert Enum.all?(alive_snakes, fn snake ->
+          previous_snake = Enum.find(board0.snakes, &(&1.id == snake.id))
+
+          Snake.head(snake) != Snake.head(previous_snake)
+        end)
+      end
+    end
+
+    property "all snakes still have the same middle body as they did on the previous turn (only heads/tails may change)", context do
+      check all board_size <- context.board_sizes,
+        {:ok, board0} <- StreamData.constant(Standard.init(board_size, @fixed_8_snake_ids)),
+        moves1 <- StreamData.list_of(StreamData.member_of(@snake_move_directions), length: 8),
+        snake_moves1 <- StreamData.constant(Enum.zip(@fixed_8_snake_ids, moves1)),
+        board1 <- StreamData.constant(Standard.next(board0, snake_moves1))
+      do
+        assert Enum.all?(board1.snakes, fn snake ->
+          [_head | rest] = snake.body
+          middle = Enum.uniq(rest)
+
+          previous_snake = Enum.find(board0.snakes, &(&1.id == snake.id))
+          [_previous_head | previous_rest] = previous_snake.body
+          previous_middle = Enum.uniq(previous_rest)
+
+          middle == previous_middle
+        end)
+      end
+    end
+
+    property "all alive snakes were also alive on the previous turn (no resurrections)", context do
+      check all board_size <- context.board_sizes,
+        {:ok, board0} <- StreamData.constant(Standard.init(board_size, @fixed_8_snake_ids)),
+        moves1 <- StreamData.list_of(StreamData.member_of(@snake_move_directions), length: 8),
+        snake_moves1 <- StreamData.constant(Enum.zip(@fixed_8_snake_ids, moves1)),
+        board1 <- StreamData.constant(Standard.next(board0, snake_moves1))
+      do
+        alive_snakes = Enum.filter(board1.snakes, &Snake.alive?/1)
+
+        assert Enum.all?(alive_snakes, fn snake ->
+          previous_snake = Enum.find(board0.snakes, &(&1.id == snake.id))
+
+          Snake.alive?(previous_snake)
+        end)
+      end
+    end
+  end
+
+  describe "next/2 for standard board sizes with 8 snakes after the first 2 turns" do
+    setup :setup_for_standard_board_sizes
+
+    property "no eliminated snakes have moved since the turn before they were eliminated (no walking dead)", context do
+      check all board_size <- context.board_sizes,
+        {:ok, board0} <- StreamData.constant(Standard.init(board_size, @fixed_8_snake_ids)),
+        moves1 <- StreamData.list_of(StreamData.member_of(@snake_move_directions), length: 8),
+        snake_moves1 <- StreamData.constant(Enum.zip(@fixed_8_snake_ids, moves1)),
+        board1 <- StreamData.constant(Standard.next(board0, snake_moves1)),
+        moves2 <- StreamData.list_of(StreamData.member_of(@snake_move_directions), length: 8),
+        snake_moves2 <- StreamData.constant(Enum.zip(@fixed_8_snake_ids, moves2)),
+        board2 <- StreamData.constant(Standard.next(board1, snake_moves2))
+      do
+        snakes_on_board2_eliminated_on_board1 = board1.snakes
+        |> Enum.filter(&Snake.eliminated?/1)
+        |> Enum.map(fn snake -> Enum.find(board2.snakes, &(&1.id == snake.id)) end)
+
+        assert Enum.all?(snakes_on_board2_eliminated_on_board1, fn snake ->
+          previous_snake = Enum.find(board1.snakes, &(&1.id == snake.id))
+
+          snake.body == previous_snake.body
+        end)
       end
     end
   end
